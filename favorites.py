15fb893e8849f0898af2e7941cc259c37057e505
Outoftheblue254 <chastainj80@gmail.com>
2025-04-01 10:35:49 -0500

    for and while loop

---
+++ favorites.py
@@ -0,0 +1,14 @@
+favorites = ['Creme Brulee', 'Apple Pie', 'Churros', 'Tiramisu', 'Chocolate Cake']
+
+for dessert in favorites:
+    print('One of my favorite desserts is ', dessert)
+    
+    
+count = 0 
+
+while count < len(favorites):
+    print('One of my favorite desserts is ', favorites[count])
+    count +=1
+    
+
+    
